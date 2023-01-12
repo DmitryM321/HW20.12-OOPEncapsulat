df2f1571d0611d9f112e10c06f78cb134ae02fa8
Dmitry <a89297175356@gmail.com>
2023-01-12 12:34:51 +0500

    ДЗ 10.01 Map

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ import transportUnit.Car;
 import transportUnit.Transport;
 import transportUnit.Truck;
 import mechanics.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,48 +20,42 @@ public class Main {
         DriverC fordov = new DriverC("Гарри Фордов", true, 111);
         DriverD ferrariev = new DriverD("Эдуард Феррариев", true, 22);
 
-        System.out.println("Задание Коллекции");
+        System.out.println("Задание Map");
         Mechanics mechanic1 = new Mechanics("Петр Первый", "Лучше не знать",  Licenses.D);
         Mechanics mechanic2 = new Mechanics("Николай Второй", "Box 231 in GSK Mechta", Licenses.B,Licenses.C);
         Mechanics mechanic3 = new Mechanics("Александр Третий", "Rolf", Licenses.C);
+        Mechanics mechanic4 = new Mechanics("Александр Третий", "Rolf", Licenses.C);
 
-        ArrayList<Transport> transports = new ArrayList<>();
-        Collections.addAll(transports, lada, audi, kamaz, kraz, liaz, mersedes);
-        System.out.println(transports);
-
-        List<Car> cars = new ArrayList<>();
-        Collections.addAll(cars, lada, audi);
-        System.out.println(cars);
-
-        List<Truck> trucks = new ArrayList<>();
-        Collections.addAll(trucks, kamaz, kraz);
-        System.out.println(trucks);
-
-        List<Bus> buses = new ArrayList<>();
-        Collections.addAll(buses, liaz, mersedes);
-        System.out.println(buses);
-
-        List<Mechanics> mechanics = new ArrayList<>();
-        Collections.addAll(mechanics, mechanic1, mechanic2, mechanic3);
-        System.out.println(mechanics);
-
-        List<Driver> drivers = new ArrayList<>();
-        Collections.addAll(drivers, porsev, fordov, ferrariev);
-        System.out.println(drivers);
-        System.out.println(mechanic2);
-        lada.setDriver(porsev);
-        lada.setMechanic(mechanic2);
-        lada.dvaVmashine();
-
-        kraz.setDriver(fordov);
-        kraz.setMechanic(mechanic3);
-        kraz.dvaVmashine();
-
-        mersedes.setDriver(ferrariev);
-        mersedes.setMechanic(mechanic1);
-        mersedes.dvaVmashine();
+        Map<Transport, Mechanics > mechanicsMap = new HashMap<>();
+        mechanicsMap.put(kamaz, mechanic1);
+        mechanicsMap.put(kraz,  mechanic1);
+        mechanicsMap.put(lada, mechanic2);
+        mechanicsMap.put(audi, mechanic2);
+        mechanicsMap.put(liaz, mechanic3);
+        mechanicsMap.put(mersedes, mechanic3);
+        mechanicsMap.put(mersedes, mechanic4); // Тестовая строка
+        System.out.println(mechanicsMap);
     }
 }
+
+//
+//
+//        List<Driver> drivers = new ArrayList<>();
+//        Collections.addAll(drivers, porsev, fordov, ferrariev);
+//        System.out.println(drivers);
+//        System.out.println(mechanic2);
+//        lada.setDriver(porsev);
+//        lada.setMechanic(mechanic2);
+//        lada.dvaVmashine();
+//
+//        kraz.setDriver(fordov);
+//        kraz.setMechanic(mechanic3);
+//        kraz.dvaVmashine();
+//
+//        mersedes.setDriver(ferrariev);
+//        mersedes.setMechanic(mechanic1);
+//        mersedes.dvaVmashine();
+
 
 //        List<Car> cars = new ArrayList<>();
 //        cars.add(lada);
@@ -106,7 +99,21 @@ public class Main {
                 throw new RuntimeException();
             }
         }*/
-
+//        ArrayList<Transport> transports = new ArrayList<>();
+//        Collections.addAll(transports, lada, audi, kamaz, kraz, liaz, mersedes);
+//        System.out.println(transports);
+//
+//        List<Car> cars = new ArrayList<>();
+//        Collections.addAll(cars, lada, audi);
+//        System.out.println(cars);
+//
+//        List<Truck> trucks = new ArrayList<>();
+//        Collections.addAll(trucks, kamaz, kraz);
+//        System.out.println(trucks);
+//
+//        List<Bus> buses = new ArrayList<>();
+//        Collections.addAll(buses, liaz, mersedes);
+//        System.out.println(buses);
 
 
 
